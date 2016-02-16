511344c7c30880c08f3bcd8f0cf1078dc64cad80
Raphael Koehler-Derrick <koehr998@newschool.edu>
2016-02-16 17:22:08 -0500

    Red & Yeller Circles

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -2,20 +2,42 @@
   Classwork
 */
 var ballX = 0;
-var speed = 3;
+var ballY = 0;
+var speed = 8;
+var speed2 = 18;
+var press = "yellow";
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
+
+
   background("white");
+  noStroke();
+  fill(press);
   ellipse(ballX, height/2, 50,50);
 
-  if (ballX >= width){
-    speed = -3;
-  }
-
+    if (ballX >= width){
+      speed = -8;
+    } else  if (ballX <= 0){
+        speed = 8;
+      }
   ballX = ballX + speed;
 
+  fill(press);
+  ellipse(width/2,ballY, 50,50);
+  if (ballY >= height){
+   speed2 = -5;
+  } else  if (ballY <= 0){
+    speed2 = 5;
+    }
+  ballY = ballY + speed2;
+
+  if(mouseIsPressed){
+  press = "red";
+  } else{
+    press = "yellow"
+    }
 }
