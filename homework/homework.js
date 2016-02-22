b4254eb4249c1f5fdef8327727775ea6e0bae8b3
Raphael Koehler-Derrick <koehr998@newschool.edu>
2016-02-22 12:20:39 -0500

    Tripped out

--- homework/homework.js
+++ homework/homework.js
@@ -3,13 +3,44 @@
 */
 
 //declare your variables
+var speed = 1/4;
+var i = 0;
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
+  frameRate(30);
 
   //set things up
 }
 
 function draw(){
-  //draw
+  background("red");
+  noStroke();
+  fill("white");
+
+for(a = 0; a < 6; a = a+1){
+    arc(width/2,height/2,100,100,a+i,a+i+PI/10);
+  }
+
+i = i + speed;
+
+  var c = color("rgba(0, 0, 255, .8)");
+  fill(c);
+
+  triangle(width/2,height/2,0,mouseY+i,width,mouseY+i);
+  triangle(width/2,height/2,0,-mouseY+height-i,width,-mouseY+height-i);
+  triangle(width/2,height/2,mouseY*2+i,0,mouseY*2+i,height);
+  triangle(width/2,height/2,-mouseY*2+width-i,0,-mouseY*2+width-i,height);
+
+  stroke(0);
+  strokeWeight(150);
+  noFill();
+  rect(0,0,width,height);
+
+  if(mouseIsPressed){
+  speed = 1/7;
+    }else{
+      speed = 1/4;
+      c = color("rgba(0, 0, 255, .8)");
+      }
 }
